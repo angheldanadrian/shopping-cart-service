202f295f893e9170f980eda7c8e3ca2417436674
Dan-Adrian Anghel <->
2019-06-28 20:30:51 +0300

    add tests

--- src/test/java/com/processor/shoppingcartservice/controller/ShoppingCartControllerTest.java
+++ src/test/java/com/processor/shoppingcartservice/controller/ShoppingCartControllerTest.java
@@ -127,4 +127,73 @@ public class ShoppingCartControllerTest {
 		verify(shoppingCartService, times(1)).insertOrUpdateShoppingCartRecords(customerEcifId,
 				shoppingCartRecords);
 	}
+
+
+	@Test
+	public void when_updatingExistingShoppingCart_Expect_ShoppingCartDocument_And_HttpStatusOK() {
+		//given
+		String customerEcifId = "5628504543";
+		String productIds = "4325435,756765876,56645654,5435,8769679,543424654";
+		MongoCartDocument expectedResult = SHOPPING_CART_DOCUMENT;
+
+		//when
+		when(shoppingCartService.updateShoppingCartRecords(customerEcifId, productIds)).thenReturn(
+				Optional.of(expectedResult));
+		ResponseEntity<MongoCartDocument> actual = shoppingCartController.updateExistingShoppingCart(customerEcifId,
+				productIds);
+
+		//then
+		assertEquals(HttpStatus.OK, actual.getStatusCode());
+		assertEquals(expectedResult, actual.getBody());
+		verify(shoppingCartService, times(1)).updateShoppingCartRecords(customerEcifId, productIds);
+	}
+
+	@Test
+	public void when_updatingExistingShoppingCart_Expect_HttpStatusNotFound() {
+		//given
+		String customerEcifId = "5628504543";
+		List<ProductModel> shoppingCartRecords = SHOPPING_CART_RECORDS;
+
+		//when
+		when(shoppingCartService.insertOrUpdateShoppingCartRecords(customerEcifId, shoppingCartRecords))
+				.thenReturn(Optional.empty());
+		ResponseEntity<MongoCartDocument> actual = shoppingCartController.addShoppingCartRecords(customerEcifId,
+				shoppingCartRecords);
+
+		//then
+		assertEquals(HttpStatus.NOT_FOUND, actual.getStatusCode());
+		assertNull(actual.getBody());
+		verify(shoppingCartService, times(1)).insertOrUpdateShoppingCartRecords(customerEcifId,
+				shoppingCartRecords);
+	}
+
+	@Test
+	public void when_deletingShoppingCartRecords_Expect_HttpStatusOK() {
+		//given
+		String customerEcifId = "5628504543";
+
+		//when
+		when(shoppingCartService.deleteByCustomerId(customerEcifId)).thenReturn(Boolean.TRUE);
+		ResponseEntity<MongoCartDocument> actual = shoppingCartController.deleteShoppingCartRecords(customerEcifId);
+
+		//then
+		assertEquals(HttpStatus.OK, actual.getStatusCode());
+		verify(shoppingCartService, times(1)).deleteByCustomerId(customerEcifId);
+	}
+
+	@Test
+	public void when_deletingShoppingCartRecords_Expect_HttpStatusNotFound() {
+		//given
+		String customerEcifId = "5628504543";
+		List<ProductModel> shoppingCartRecords = SHOPPING_CART_RECORDS;
+
+		//when
+		when(shoppingCartService.deleteByCustomerId(customerEcifId)).thenReturn(Boolean.FALSE);
+		ResponseEntity<MongoCartDocument> actual = shoppingCartController.deleteShoppingCartRecords(customerEcifId);
+
+		//then
+		assertEquals(HttpStatus.NOT_FOUND, actual.getStatusCode());
+		assertNull(actual.getBody());
+		verify(shoppingCartService, times(1)).deleteByCustomerId(customerEcifId);
+	}
 }
